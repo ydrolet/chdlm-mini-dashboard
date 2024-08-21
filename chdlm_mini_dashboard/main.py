10e0d78c910e4d7b7fea0e123010d8bb6c181eb6
Yannick Drolet <ydrolet@gmail.com>
2024-08-20 22:45:33 -0400

    Validate data coming from the data extraction script

--- chdlm_mini_dashboard/main.py
+++ chdlm_mini_dashboard/main.py
@@ -1,8 +1,11 @@
 from chdlm_mini_dashboard.clients.google_api import GoogleApiClient
+from chdlm_mini_dashboard.models.chdlm_involvement_data import ScriptResponse
 from chdlm_mini_dashboard.settings import settings
 
 google_api_client = GoogleApiClient(settings.token_file_path, ["https://www.googleapis.com/auth/spreadsheets"])
 
 response = google_api_client.execute_script_function(settings.script_id, "extractData")
 
-print(response)
+validated_response = ScriptResponse.model_validate(response["response"]["result"])
+
+print(validated_response)
---
+++ chdlm_mini_dashboard/models/chdlm_involvement_data.py
@@ -0,0 +1,60 @@
+from enum import Enum
+from typing import Annotated
+
+from pydantic import Field, field_validator, model_validator
+from pydantic_extra_types.pendulum_dt import Duration, DateTime
+from typing_extensions import Self
+
+from chdlm_mini_dashboard.models.common import CustomBaseModel
+
+class ExecutionTime(CustomBaseModel):
+    spreadsheet_loading: Duration
+    data_extraction: Duration
+
+    @field_validator("*", mode="before")
+    def transform(cls, v) -> str:
+        assert isinstance(v, float), 'Input value must be a float'
+        return f"PT{v}S"
+
+class LogLevel(str, Enum):
+    info = "info"
+    warning = "warning"
+    error = "error"
+
+class LogEntry(CustomBaseModel):
+    timestamp: DateTime
+    message: str
+    level: LogLevel
+
+class Address(CustomBaseModel):
+    civic_number: int
+    unit_number: int
+
+class MemberStatus(str, Enum):
+    member = "Membre"
+    probation = "Probation"
+    non_member = "Non-membre"
+    former = "Ancien"
+
+class AccomplishedTask(CustomBaseModel):
+    task_name: str
+    hours_spent: float
+    note: str
+
+class Resident(CustomBaseModel):
+    first_name: str
+    last_name: str
+    address: Address | None = None
+    member_status: MemberStatus
+    involvement: dict[int, dict[Annotated[int, Field(ge=1, le=12)], dict[str, list[AccomplishedTask]]]]
+
+    @model_validator(mode='after')
+    def check_former_member_address(self) -> Self:
+        if self.member_status == MemberStatus.former and self.address is not None:
+            raise ValueError('Address must be null for former members')
+        return self
+
+class ScriptResponse(CustomBaseModel):
+    data: dict[str, Resident] | None = None
+    log: list[LogEntry]
+    execution_time: ExecutionTime
