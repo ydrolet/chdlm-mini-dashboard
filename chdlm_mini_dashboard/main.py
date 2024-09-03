a12841d6fc035dda5c66a92f6a8b09b432fed341
Yannick Drolet <ydrolet@gmail.com>
2024-09-02 22:41:29 -0400

    Update main.py

--- chdlm_mini_dashboard/main.py
+++ chdlm_mini_dashboard/main.py
@@ -1,17 +1,22 @@
+import locale
 import logging
 
 from chdlm_mini_dashboard.clients.google_api import GoogleApiClient
 from chdlm_mini_dashboard.managers.chdlm_involvement import ChdlmInvolvementManager
 from chdlm_mini_dashboard.settings import settings
 
+locale.setlocale(locale.LC_ALL, 'fr_CA')
+
 logging.basicConfig(level=logging.INFO)
 
-google_api_client = GoogleApiClient(settings.token_file_path, ["https://www.googleapis.com/auth/spreadsheets"])
-chdlm_involvement_manager = ChdlmInvolvementManager(
-    google_api_client, settings.chdlm_involvement_data_extraction_script_id)
+google_api_client = GoogleApiClient(
+    settings.token_file_path,
+    ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/gmail.send"],
+)
+chdlm_involvement_manager = ChdlmInvolvementManager(google_api_client,
+                                                    settings.chdlm_involvement_data_extraction_script_id)
 
 residents = chdlm_involvement_manager.get_residents_list()
-print(residents)
+residents_names = sorted(list(residents.keys()))
 
 involvement_data = chdlm_involvement_manager.get_involvement_data()
-print(involvement_data)
