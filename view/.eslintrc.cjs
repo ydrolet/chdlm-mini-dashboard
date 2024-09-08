d5cac28db8b3cc1e6891581a43bc8fa3074033a2
Yannick Drolet <ydrolet@gmail.com>
2024-09-08 16:56:17 -0400

    Change linter rules

--- view/.eslintrc.cjs
+++ view/.eslintrc.cjs
@@ -71,8 +71,6 @@ module.exports = {
 
     'prefer-promise-reject-errors': 'off',
 
-    quotes: ['warn', 'single', { avoidEscape: true }],
-
     // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
 
