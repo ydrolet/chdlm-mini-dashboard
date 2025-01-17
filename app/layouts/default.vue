ceb68f831b31996f4cce3cc6cb183372e2d9b985
Yannick Drolet <ydrolet@gmail.com>
2025-01-17 16:23:17 -0500

    Change footer and add involvement summary email example

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -8,10 +8,15 @@
       <slot name="content" />
     </div>
 
-    <div class="flex items-center text-right text-sm pt-14 text-gray-500">
-      <div>Fait et maintenu par<br><b>Yannick Drolet</b></div>
-      <NuxtLink to="https://github.com/ydrolet/chdlm-mini-dashboard">
-        <i class="pi pi-github pl-2.5" style="font-size: 1.5rem" />
+    <div class="flex flex-col items-center gap-5 text-sm pt-14 text-gray-500">
+      <div class="flex flex-col items-center text-sm">
+        <div>Fait et maintenu par</div>
+        <div class="font-bold">Yannick Drolet</div>
+      </div>
+
+      <NuxtLink to="https://github.com/ydrolet/chdlm-mini-dashboard" class="flex flex-row items-center gap-1.5">
+        <i class="pi pi-github pl-2.5" style="font-size: 1.3rem" />
+        <span class="text-xs">Code source</span>
       </NuxtLink>
     </div>
   </div>
